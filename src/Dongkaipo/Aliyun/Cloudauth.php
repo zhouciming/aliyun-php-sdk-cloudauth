0b3821c16144f3bde7c83ce47700dcf0d31f3451
Jack <dongkaipo@126.com>
2018-09-30 17:15:06 +0800

    init
    Signed-off-by: Jack <dongkaipo@126.com>

---
+++ src/Dongkaipo/Aliyun/Cloudauth.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jack
+ * Date: 2018/9/30
+ * Time: 2:25 PM
+ */
+
+namespace Dongkaipo\Aliyun;
+
+class Cloudauth
+{
+    public function __construct()
+    {
+
+    }
+
+}
